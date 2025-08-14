7c6f7b913e7e28d3dd998bb8ae8d4fd1345b5f0a
Khush Manchanda <manchandakhush@gmail.com>
2025-08-13 19:00:38 -0700

    Initial Commit

---
+++ Zyn Tracking/Zyn_TrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zyn_TrackingApp.swift
+//  Zyn Tracking
+//
+//  Created by Khush Manchanda on 8/13/25.
+//
+
+import SwiftUI
+
+@main
+struct Zyn_TrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Zyn TrackingTests/Zyn_TrackingTests.swift
@@ -0,0 +1,16 @@
+//
+//  Zyn_TrackingTests.swift
+//  Zyn TrackingTests
+//
+//  Created by Khush Manchanda on 8/13/25.
+//
+
+import Testing
+
+struct Zyn_TrackingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
